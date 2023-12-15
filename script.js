6db74aa89a2ca6b46e862cfc5a45d96923a84c76
Santiago Fernandez <109321011+SearPy@users.noreply.github.com>
2023-12-15 14:51:35 -0300

    funcional

--- script.js
+++ script.js
@@ -1,68 +1,122 @@
 
-async function cargarPersonajeAleatorio() {
-    const personaje = await obtenerPersonajeAleatorio();
-    const cuestionarioHTML = generarCuestionario(personaje);
-    document.getElementById("quiz-container").innerHTML = cuestionarioHTML;
-}
+var person;
+var alt = 1;
 
-async function obtenerPersonajeAleatorio() {
-    const id = Math.floor(Math.random() * 5) + 1;
-    const response = await fetch(`https://swapi.dev/api/people/${id}/`);
-    const personaje = await response.json();
-    return personaje;
-}
+var num = Math.floor(Math.random() * 5) + 1;
+console.log(num)
 
-function generarOpcionesIncorrectasColorOjos(personaje) {
-    const coloresPosibles = ["verde", "gris", "marrón", "amarillo"];
-    const opcionesIncorrectas = coloresPosibles.filter(color => color !== personaje.eye_color);
-    return opcionesIncorrectas.slice(0, 2);
-}
+const endpoint = "https://swapi.dev/api/people/"+num+"/"
+var nom = document.getElementById("personaje")
+var pre = document.getElementById("pregunta")
+var op = document.getElementById("opcion")
 
-function generarOpcionesIncorrectasNombreCompleto(personaje) {
-    const partesNombre = personaje.name.split(" ");
-    const nombreIncorrecto1 = partesNombre.reverse().join(" ");
-    return [nombreIncorrecto1];
-}
-function generarOpcionesIncorrectas(personaje){
-    const presencias = personaje.films.length
-    return[presencias + 1, presencias - 1]
-}
-
-function generarCuestionario(personaje) {
-    const preguntas = [
-        {
-            pregunta: `¿Cuál es el nombre completo de ${personaje.name.split(" ")[0]}?`,
-            opciones: generarOpcionesIncorrectasNombreCompleto(personaje).concat([personaje.name]),
-        },
-        {
-            pregunta: `¿En cuantas peliculas de Star Wars estuvo presente ${personaje.name}?`,
-            opciones: generarOpcionesIncorrectas(personaje).concat([personaje.films.length]),
-        },
-        {
-            pregunta: `¿Cuál es el color de ojos de ${personaje.name}?`,
-            opciones: generarOpcionesIncorrectasColorOjos(personaje).concat([personaje.eye_color]),
-        },
-    ];
-
-    let cuestionarioHTML = "<h2>Cuestionario sobre " + personaje.name + "</h2>";
-    cuestionarioHTML += "<ol>";
-    
-    preguntas.forEach((item, index) => {
-        cuestionarioHTML += `<li>${item.pregunta}`;
-        cuestionarioHTML += "<ul>";
-        item.opciones.forEach(opcion => {
-            cuestionarioHTML += `<li>${opcion}</li>`;
+function cargarPersonaje(){
+    fetch(endpoint).then((response) =>{
+        response.json().then((data) => {
+            person = data
+            console.log(person)
+            nom.innerHTML = "<h3>"+person.name.toUpperCase()+"</h3>"
+            alt = data.height
+            pregunta()
         });
-        cuestionarioHTML += "</ul></li>";
     });
-
-    cuestionarioHTML += "</ol>";
-
-    return cuestionarioHTML;
+     
 }
 
 
+cargarPersonaje()
+var preg = 1;
 
 
+function pregunta(){
+    var a;
+    var b;
+    var c;
+    if(preg == 1){
+        a = alt/100
+        b = (alt - 10)/100
+        c = (parseInt(alt)+ 10)/100;
+        pre.innerHTML = "<h2>Cual es su altura?</h2>"
+        op.innerHTML = "<p> A) "+a+"m    B) "+b+ "m   C) "+c+"m</P"
+    }else if(preg == 2){
+        a = "Verdes";
+        if(person.eye_color == "blue"){
+            b = "Azules";
+        }else if(person.eye_color == "yelLow"){
+            b = "Amarillos"
+        }else if(person.eye_color == "red"){
+            b = "Rojo"
+        }else{
+            b = "Marrones"
+        }
+        var c = "Negros";
+        pre.innerHTML = "<h2>Cual es el color de sus ojos?</h2>"
+        op.innerHTML = "<p> A) "+a+"    B) "+b+ "   C) "+c+"</P"
+    }else if(preg ==3){
+        a = 1
+        b = 2
+        c = person.films.length
+        pre.innerHTML = "<h2>En cuantas peliculas aparecio?</h2>"
+        op.innerHTML = "<p> A) "+a+"    B) "+b+ "   C) "+c+"</P"
+    }else if(preg ==4){
+        a = person.starships.length;
+        b = (person.starships.length)+1;
+        c = (person.starships.length)+2;
+        pre.innerHTML = "<h2>Cuantas naves ha pilotado?</h2>"
+        op.innerHTML = "<p> A) "+a+"m    B) "+b+ "m   C) "+c+"m</P"
+    }
+}
+
+const buttona = document.getElementById("opcionA");
+const buttonb = document.getElementById("opcionB");
+const buttonc = document.getElementById("opcionC");
+const siguiente = document.getElementById("siguiente");
+var respuesta = document.getElementById("correcto");
+var puntuacion = document.getElementById("puntos");
+var puntos = 0;
+
+buttona.addEventListener('click', () => {
+    if(preg == 1 || preg == 4){
+        respuesta.innerHTML = "<h1> Correcto! </h1>"
+        puntos ++
+        if(preg == 4){
+          puntuacion.innerHTML = "<h2> Puntuacion final: "+puntos+"</h2>"
+          siguiente.disabled = true;
+          buttona.disabled = true;
+          buttonb.disabled = true;
+          buttonc.disabled = true;
+        }
+    }else{
+        respuesta.innerHTML = "<h1> Fallaste! </h1>"
+    }
+    preg ++
+});
+
+buttonb.addEventListener('click', () => {
+    if(preg == 2){
+        respuesta.innerHTML = "<h1> Correcto! </h1>"
+        puntos ++
+    }else{
+        respuesta.innerHTML = "<h1> Fallaste! </h1>"
+    }
+    preg ++
+});
+
+buttonc.addEventListener('click', () => {
+    if(preg == 3){
+        respuesta.innerHTML = "<h1> Correcto! </h1>"
+        puntos ++
+    }else{
+        respuesta.innerHTML = "<h1> Fallaste! </h1>"
+    }
+    preg ++
+});
+
+siguiente.addEventListener('click', () => {
+    console.log(preg)
+    pregunta();
+    respuesta.innerHTML = "<h1></h1>"
+    
+})
 
 
