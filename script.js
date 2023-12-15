7e869881d3ae78c6da84f05fa4096e30642f2618
aleSolis2001 <118699950+aleSolis2001@users.noreply.github.com>
2023-12-15 04:53:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,66 @@
 
-var person;
+async function cargarPersonajeAleatorio() {
+    const personaje = await obtenerPersonajeAleatorio();
+    const cuestionarioHTML = generarCuestionario(personaje);
+    document.getElementById("quiz-container").innerHTML = cuestionarioHTML;
+}
 
-var num = Math.floor(Math.random() * 5) + 1;
-console.log(num)
+async function obtenerPersonajeAleatorio() {
+    const id = Math.floor(Math.random() * 5) + 1;
+    const response = await fetch(`https://swapi.dev/api/people/${id}/`);
+    const personaje = await response.json();
+    return personaje;
+}
 
-const endpoint = "https://swapi.dev/api/people/"+num+"/"
+function generarOpcionesIncorrectasColorOjos(personaje) {
+    const coloresPosibles = ["verde", "gris", "marrón", "amarillo"];
+    const opcionesIncorrectas = coloresPosibles.filter(color => color !== personaje.eye_color);
+    return opcionesIncorrectas.slice(0, 2);
+}
 
-fetch(endpoint).then((response) =>{
-    response.json().then((data) => { 
-        person = data
-        console.log(person)
+function generarOpcionesIncorrectasNombreCompleto(personaje) {
+    const partesNombre = personaje.name.split(" ");
+    const nombreIncorrecto1 = partesNombre.reverse().join(" ");
+    return [nombreIncorrecto1];
+}
+function generarOpcionesIncorrectas(personaje){
+    const presencias = personaje.films.length
+    return[presencias + 1, presencias - 1]
+}
+
+function generarCuestionario(personaje) {
+    const preguntas = [
+        {
+            pregunta: `¿Cuál es el nombre completo de ${personaje.name.split(" ")[0]}?`,
+            opciones: generarOpcionesIncorrectasNombreCompleto(personaje).concat([personaje.name]),
+        },
+        {
+            pregunta: `¿En cuantas peliculas de Star Wars estuvo presente ${personaje.name}?`,
+            opciones: generarOpcionesIncorrectas(personaje).concat([personaje.films.length]),
+        },
+        {
+            pregunta: `¿Cuál es el color de ojos de ${personaje.name}?`,
+            opciones: generarOpcionesIncorrectasColorOjos(personaje).concat([personaje.eye_color]),
+        },
+    ];
+
+    let cuestionarioHTML = "<h2>Cuestionario sobre " + personaje.name + "</h2>";
+    cuestionarioHTML += "<ol>";
+    
+    preguntas.forEach((item, index) => {
+        cuestionarioHTML += `<li>${item.pregunta}`;
+        cuestionarioHTML += "<ul>";
+        item.opciones.forEach(opcion => {
+            cuestionarioHTML += `<li>${opcion}</li>`;
+        });
+        cuestionarioHTML += "</ul></li>";
     });
-});
+
+    cuestionarioHTML += "</ol>";
+
+    return cuestionarioHTML;
+}
+
 
 
 
